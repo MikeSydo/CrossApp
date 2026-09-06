96a70d3deffd995493e86b027d1b814b110ed250
MikeSydo <mikesydocompan@gmail.com>
2026-09-06 15:59:04 +0300

    lab01: solution CrossApp, Cli, Orders

---
+++ src/Cli/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+Console.WriteLine("CrossApp – практикум з крос-платформного програмування");
+Console.WriteLine("Студент: Сидорук Михайло, група ФЕІ-32с");
+Console.WriteLine(new string('-', 52));
+Console.WriteLine($"ОС (OSDescription) : {RuntimeInformation.OSDescription}");
+Console.WriteLine($"ОС (Environment) : {Environment.OSVersion}");
+Console.WriteLine($"Архітектура процесу : {RuntimeInformation.ProcessArchitecture}");
+Console.WriteLine($"Версія .NET (CLR) : {Environment.Version}");
+Console.WriteLine($"Runtime : {RuntimeInformation.FrameworkDescription}");
+Console.WriteLine($"Каталог застосунку : {AppContext.BaseDirectory}");
+Console.WriteLine($"Поточний каталог : {Environment.CurrentDirectory}");
+Console.WriteLine(new string('-', 52));
+Console.WriteLine("Предметна область: Замовлення (клієнти, товари, замовлення, рядки замовлень)");
